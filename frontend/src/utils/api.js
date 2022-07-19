d037fce4645010f9cc9dc6268e8f4992981748e8
Anna Mikheeva <Lishudi@yandex.ru>
2022-07-19 23:45:19 +0300

    support of http and https

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -110,10 +110,13 @@ class Api {
   }
 };
 
+const currentBaseUrl = document.location.protocol !== 'https:' ? 'http://api.mesto.annam.nomoredom.nomoredomains.xyz' : 'https://api.mesto.annam.nomoredom.nomoredomains.xyz';
+
 export const api = new Api({
   // baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-39',
   // baseUrl: 'http://localhost:3000',
-  baseUrl: 'https://api.mesto.annam.nomoredom.nomoredomains.xyz',
+  // baseUrl: 'https://api.mesto.annam.nomoredom.nomoredomains.xyz',
+  baseUrl: currentBaseUrl,
   headers: {
     'Content-Type': 'application/json'
   }
