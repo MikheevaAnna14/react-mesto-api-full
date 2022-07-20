dd2fd1c101f6214211959f3e45bb3f08ea2deb1a
Anna Mikheeva <Lishudi@yandex.ru>
2022-07-20 15:04:13 +0300

    add errorhandler

--- backend/app.js
+++ backend/app.js
@@ -6,21 +6,8 @@ const { errors } = require('celebrate');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
-// const signupValidation = require('./middlewares/signupValidation');
-// const signinValidation = require('./middlewares/signinValitation');
-
-// const {
-//   login,
-//   logout,
-//   createUser,
-// } = require('./controllers/users');
-
-// const auth = require('./middlewares/auth');
 const cors = require('./middlewares/cors');
-
-// const routerUsers = require('./routes/users');
-// const routerCards = require('./routes/cards');
-// const NotFoundError = require('./errors/NotFoundError');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -39,28 +26,22 @@ app.get('/crash-test', () => {
   }, 0);
 });
 app.use(routes);
-// app.post('/signup', signupValidation, createUser);
-// app.post('/signin', signinValidation, login);
-// app.post('/signout', logout);
-// app.use('/users', auth, routerUsers);
-// app.use('/cards', auth, routerCards);
-
-// app.use('*', auth, (req, res, next) => next(new NotFoundError('Несуществующий путь')));
 
 app.use(errorLogger);
 app.use(errors()); // обработчик ошибок celebreate
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  res
-    .status(err.statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибкa'
-        : err.message,
-    });
-  next();
-});
+app.use(errorHandler);
+// app.use((err, req, res, next) => {
+//   const { statusCode = 500 } = err;
+//   res
+//     .status(err.statusCode)
+//     .send({
+//       message: statusCode === 500
+//         ? 'На сервере произошла ошибкa'
+//         : err.message,
+//     });
+//   next();
+// });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
